616b35b5cfa93e49fdf1c902704d76c8e33dd05a
Harley Trung <harley@bsa.edu.vn>
2015-12-16 11:36:30 +0700

    milestone 1

---
+++ TinderLab/CardsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderLab
+//
+//  Created by Harley Trung on 12/16/15.
+//  Copyright © 2015 cheetah.com. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
