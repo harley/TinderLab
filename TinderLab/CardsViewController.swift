e0170dacd56fe431e870fd6422fee6ca75599c6f
Harley Trung <harley@bsa.edu.vn>
2015-12-16 12:02:56 +0700

    milestone 2

--- TinderLab/CardsViewController.swift
+++ TinderLab/CardsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardsViewController.swift
 //  TinderLab
 //
 //  Created by Harley Trung on 12/16/15.
@@ -10,16 +10,43 @@ import UIKit
 
 class CardsViewController: UIViewController {
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBAction func onPan(sender: UIPanGestureRecognizer) {
+        let location = sender.locationInView(view)
+        if sender.state == .Began {
+            originalCenter = profileImageView.center
+        } else if sender.state == .Changed {
+            profileImageView.center.x = location.x
+        } else if sender.state == .Ended {
+            profileImageView.center = originalCenter
+        }
+    }
+    
+    var originalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        originalCenter = profileImageView.frame.origin
+        
     }
     
     
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
 }
-
