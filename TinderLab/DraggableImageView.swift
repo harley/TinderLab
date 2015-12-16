fc7c74fc08472e8015fec7508530eff4475f6bcb
Harley Trung <harley@bsa.edu.vn>
2015-12-16 15:16:38 +0700

    milestone 4 and 5

--- TinderLab/DraggableImageView.swift
+++ TinderLab/DraggableImageView.swift
@@ -12,13 +12,41 @@ class DraggableImageView: UIView {
     var originalCenter: CGPoint!
     
     @IBAction func onPan(sender: UIPanGestureRecognizer) {
+        guard !self.imageView.hidden else { return }
+        
         let location = sender.locationInView(superview)
+        let translation = sender.translationInView(superview)
+        
         if sender.state == .Began {
             originalCenter = imageView.center
         } else if sender.state == .Changed {
-            imageView.center.x = location.x
+            center.x = location.x
+            
+            let angle:CGFloat = translation.x * 30.0 / 304
+            let coeff:CGFloat = location.y > (64 + 152) ? -1 : 1
+            let angleInRadians = coeff * angle * CGFloat(M_PI) / 180.0
+            transform = CGAffineTransformMakeRotation(angleInRadians)
+            
         } else if sender.state == .Ended {
-            imageView.center = originalCenter
+            if translation.x > 50 {
+                // animate the photo off the screen to the right
+                UIView.animateWithDuration(1, animations: { () -> Void in
+                    self.center.x += self.frame.width
+                    }, completion: { (finished) -> Void in
+                        self.hidden = true
+                })
+            } else if translation.x < -50 {
+                // animate the photo off the screen to the left
+                UIView.animateWithDuration(1, animations: { () -> Void in
+                    self.imageView.center.x -= self.frame.width
+                    }, completion: { (finished) -> Void in
+                        self.imageView.hidden = true
+                        
+                })
+            } else {
+                imageView.center = originalCenter
+                imageView.transform = CGAffineTransformIdentity
+            }
         }
     }
     /*
